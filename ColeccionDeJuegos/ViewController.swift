56301078d9f152d3cccdbe1b67e7ec060dd24d9d
Adrian Palacios Medrano <adrianpalaciosmedrano@Adrian--MacBook-Air-de-mbtec22.local>
2019-10-11 17:17:08 -0500

    first commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Adrian Palacios Medrano on 10/10/19.
+//  Copyright © 2019 Adrian Palacios Medrano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return juegos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: (juego.imagen!) as Data)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegoViewController
+        siguienteVC.juego = sender as? Juego
+    }
+    
+    var juegos: [Juego] = []
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            try juegos = context.fetch(Juego.fetchRequest())
+            tableView.reloadData()
+        }catch{
+            
+        }
+        
+    }
+    
+
+
+}
+
